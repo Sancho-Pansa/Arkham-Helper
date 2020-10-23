c19b7380ad5137695a32beabaece7d5cbd7edeba
SanchoPanzsa <zelgadis4@gmail.com>
2020-10-24 00:23:10 +0300

    First iteration of AO Editor

--- src/arkham/editor/AncientEditor.java
+++ src/arkham/editor/AncientEditor.java
@@ -2,6 +2,7 @@ package arkham.editor;
 
 import arkham.json.*;
 import arkham.mechanics.*;
+import org.apache.commons.cli.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -10,6 +11,27 @@ import java.util.Scanner;
 public class AncientEditor {
 
     public static void main(String[] args) {
+        // CLI options part
+        Option customFilepath = Option.builder("f")
+                .desc("Custom JSON filepath")
+                .numberOfArgs(1)
+                .type(String.class)
+                .build();
+        Options opts = new Options();
+        opts.addOption(customFilepath);
+        CommandLineParser clp = new DefaultParser();
+        String filepath = "resources/AncientOnes.json";
+
+        try {
+            CommandLine line = clp.parse(opts, args);
+            if(line.hasOption("f"))
+                filepath = line.getOptionValue("f");
+        } catch(ParseException e) {
+            System.err.println("Error during CLI parser");
+            e.printStackTrace();
+        }
+
+        // Survey part
         Scanner sc = new Scanner(System.in);
         System.out.print("Имя: ");
         String name = sc.nextLine();
@@ -44,5 +66,16 @@ public class AncientEditor {
         );
 
         System.out.println(ao.toString());
+        System.out.println("Correct? (y/n)");
+        if(sc.nextLine().contains("n")) {
+            System.out.println("Aborted");
+            return;
+        }
+
+        sc.close();
+
+        // JSON part
+        boolean resultFlag = AncientOneToJson.convertAncientOne(ao, filepath);
+        System.out.println(resultFlag);
     }
 }
