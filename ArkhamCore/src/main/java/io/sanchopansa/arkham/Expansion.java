6b9117deb85d4378cc52516793a6bca3f056c8df
SanchoPansa <zelgadis4@gmail.com>
2024-09-11 13:50:44 +0300

    Added localized parameters and valueOf variant

--- ArkhamCore/src/main/java/io/sanchopansa/arkham/Expansion.java
+++ ArkhamCore/src/main/java/io/sanchopansa/arkham/Expansion.java
@@ -1,11 +1,31 @@
 package io.sanchopansa.arkham;
 
 public enum Expansion {
-    VANILLA,
-    CotDP, // Curse of the Dark Pharaoh
-    DUNWICH,
-    TKiY, // The King in Yellow
-    KINGSPORT,
-    INNSMOUTH,
-    MISKATONIC
+    VANILLA("Ужас Аркхэма"),
+    CotDP("Проклятие Тёмного Фараона"), // Curse of the Dark Pharaoh
+    DUNWICH("Ужас Данвича"),
+    TKiY("Корлоль в Жёлтом"), // The King in Yellow
+    KINGSPORT("Ужас Кингспорта"),
+    INNSMOUTH("Ужас Иннсмута"),
+    MISKATONIC("Ужас Мискатоника"),
+    TBGofW("Чёрная Коза Чащоб"), // The Black Goat of the Woods
+    TLatT("Затаившийся у Порога"); // The Lurker at the Threshold
+
+    private final String expansionName;
+    Expansion(String name) {
+        this.expansionName = name;
+    }
+
+    public String getExpansionName() {
+        return expansionName;
+    }
+
+    public static Expansion fromString(String name) {
+        for(Expansion e: Expansion.values()) {
+            if(name.equalsIgnoreCase(e.expansionName)) {
+                return e;
+            }
+        }
+        return Expansion.VANILLA;
+    }
 }
