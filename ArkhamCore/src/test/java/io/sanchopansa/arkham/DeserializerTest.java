dabf8f334f74506852d75c2f6d9504ca5621feed
SanchoPansa <zelgadis4@gmail.com>
2024-09-17 18:11:44 +0300

    DeserializerTest changes

--- ArkhamCore/src/test/java/io/sanchopansa/arkham/DeserializerTest.java
+++ ArkhamCore/src/test/java/io/sanchopansa/arkham/DeserializerTest.java
@@ -1,9 +1,12 @@
 package io.sanchopansa.arkham;
 
 import com.google.gson.GsonBuilder;
-import io.sanchopansa.arkham.cards.CommonItem;
-import io.sanchopansa.arkham.deserializers.InvestigatorDeserializer;
+import io.sanchopansa.arkham.cards.*;
+import io.sanchopansa.arkham.deserializers.*;
 import io.sanchopansa.arkham.investigators.Investigator;
+import io.sanchopansa.arkham.monsters.*;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -14,39 +17,58 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DeserializerTest {
-    static Investigator[] investigators;
+    static GsonBuilder gBuilder;
     static CommonItem[] commonItems;
+    static Monster[] monsters;
 
     @BeforeAll
     public static void setUpBeforeClass() throws IOException, URISyntaxException {
-        GsonBuilder gson = new GsonBuilder();
-        var investigatorCollection = new DeserializerTest()
-                .getStreamFromResourcesFile("Investigators.json")
-                .collect(Collectors.joining());
-        gson.registerTypeAdapter(Investigator.class, new InvestigatorDeserializer());
-        investigators = gson.create().fromJson(investigatorCollection, Investigator[].class);
-
-        var commonItemsJson = new DeserializerTest()
-                .getStreamFromResourcesFile("CommonItems.json")
-                .collect(Collectors.joining());
-        gson.registerTypeAdapter(Investigator.class, new InvestigatorDeserializer());
-        commonItems = gson.create().fromJson(commonItemsJson, CommonItem[].class);
+        gBuilder = new GsonBuilder();
+    }
+    
+    @AfterEach
+    public void tearDown() {
+        gBuilder = new GsonBuilder();
     }
 
     @Test
-    public void InvestigatorDeserializationTest() {
+    public void InvestigatorDeserializationTest() throws IOException, URISyntaxException {
+        var investigatorCollection = new DeserializerTest()
+                .getStreamFromResourcesFile("Investigators.json")
+                .collect(Collectors.joining());
+        gBuilder.registerTypeAdapter(Investigator.class, new InvestigatorDeserializer());
+        var investigators = gBuilder.create().fromJson(investigatorCollection, Investigator[].class);
         assertTrue(investigators.length > 0, "Array of investigators is not empty");
     }
 
     @Test
-    public void CommonItemDeserializationTest() {
+    public void CommonItemDeserializationTest() throws IOException, URISyntaxException {
+        var commonItemsJson = new DeserializerTest()
+                .getStreamFromResourcesFile("CommonItems.json")
+                .collect(Collectors.joining());
+        gBuilder.registerTypeAdapter(CommonItem.class, new CommonItemDeserializer());
+        var commonItems = gBuilder.create().fromJson(commonItemsJson, CommonItem[].class);
         assertTrue(commonItems.length > 0, "Array of Common Items is not empty");
+    }
+
+    @Test
+    public void MonsterDeserializationTest() throws IOException, URISyntaxException {
+        var monsterCollection = new DeserializerTest()
+                .getStreamFromResourcesFile("Monsters.json")
+                .collect(Collectors.joining());
+        gBuilder.registerTypeAdapter(Monster.class, new MonsterDeserializer());
+        var monsters = gBuilder.create().fromJson(monsterCollection, Monster[].class);
+
+        assertTrue(monsters.length > 0);
+        System.out.println(monsters[0].toString());
+        assertTrue(Arrays.stream(monsters).anyMatch((monster -> monster.getName().equals("Культист"))));
     }
 
     /**
