68b725962523fd7a0db21ce8ed72d9fb36f7fc45
SanchoPansa <zelgadis4@gmail.com>
2024-09-24 09:27:36 +0300

    Changes in SkillCard class

--- ArkhamCore/src/main/java/io/sanchopansa/arkham/cards/SkillCard.java
+++ ArkhamCore/src/main/java/io/sanchopansa/arkham/cards/SkillCard.java
@@ -1,11 +1,17 @@
 package io.sanchopansa.arkham.cards;
 
+import io.sanchopansa.arkham.ActiveEffect;
 import io.sanchopansa.arkham.Expansion;
+import io.sanchopansa.arkham.Phase;
 
 public class SkillCard extends AbstractCard {
     private static final CardType CARD_TYPE = CardType.SKILL;
 
-    public SkillCard(Expansion e, String name, String description) {
-        super(e, name, CARD_TYPE);
+    public SkillCard(Expansion e, String name, BonusStats bonusStats, Phase phaseToUse, String active, String passive) {
+        super(e, name, CARD_TYPE, bonusStats, active, phaseToUse, passive);
+    }
+
+    public SkillCard(Expansion e, String name, BonusStats bonusStats, ActiveEffect active, String passive) {
+        super(e, name, CARD_TYPE, bonusStats, active, passive);
     }
 }
