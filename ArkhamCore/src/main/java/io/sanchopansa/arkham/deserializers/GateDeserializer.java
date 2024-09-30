87c415b8a428feae304ae9c2ba2dcee0f35cc9b0
SanchoPansa <zelgadis4@gmail.com>
2024-09-30 16:08:58 +0300

    Gate class expansion

---
+++ ArkhamCore/src/main/java/io/sanchopansa/arkham/deserializers/GateDeserializer.java
@@ -0,0 +1,30 @@
+package io.sanchopansa.arkham.deserializers;
+
+import com.google.gson.*;
+import io.sanchopansa.arkham.Expansion;
+import io.sanchopansa.arkham.monsters.DimensionSymbol;
+import io.sanchopansa.arkham.monsters.Gate;
+
+import java.lang.reflect.Type;
+
+public class GateDeserializer extends AbstractDeserializer implements JsonDeserializer<Gate> {
+    @Override
+    public Gate deserialize(JsonElement jsonElement,
+                            Type type,
+                            JsonDeserializationContext jsonDeserializationContext
+    ) throws JsonParseException {
+        JsonObject jsonMonster = jsonElement.getAsJsonObject();
+
+        String world = jsonMonster.get("name").getAsString();
+        Expansion e = Expansion.fromString(jsonMonster.get("expansion").getAsString());
+        byte modifier = jsonMonster.get("modifier").getAsByte();
+        DimensionSymbol symbol = DimensionSymbol.of(jsonMonster.get("symbol").getAsString());
+
+        return new Gate(
+                e,
+                world,
+                modifier,
+                symbol
+        );
+    }
+}
--- ArkhamCore/src/main/java/io/sanchopansa/arkham/monsters/Gate.java
+++ ArkhamCore/src/main/java/io/sanchopansa/arkham/monsters/Gate.java
@@ -1,13 +1,79 @@
 package io.sanchopansa.arkham.monsters;
 
+import io.sanchopansa.arkham.AbstractGameElement;
 import io.sanchopansa.arkham.Expansion;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.Objects;
 
 /**
  * Класс-рекорд, описывающий жетон Врат
- * @param world Иной мир
- * @param modifier Модификатор сложности закрытия
- * @param dSymbol Символ Иного мира
  */
-public record Gate(String world, int modifier, DimensionSymbol dSymbol, Expansion expansion) {
+public final class Gate extends AbstractGameElement implements Cloneable {
+    private final String world;
+    private final byte modifier;
+    private final DimensionSymbol symbol;
 
+    /**
+     * @param world    Иной мир
+     * @param modifier Модификатор сложности закрытия
+     * @param dSymbol  Символ Иного мира
+     */
+    public Gate(Expansion e, String world, byte modifier, DimensionSymbol dSymbol) {
+        super(e);
+        this.world = world;
+        this.modifier = modifier;
+        this.symbol = dSymbol;
+    }
+
+    public Gate(Gate original) {
+        super(original.expansion);
+        this.world = original.world;
+        this.modifier = original.modifier;
+        this.symbol = original.symbol;
+    }
+
+    public String getWorld() {
+        return world;
+    }
+
+    public byte getModifier() {
+        return modifier;
+    }
+
+    public DimensionSymbol getDimension() {
+        return symbol;
+    }
+
+    @Override
+    public Gate clone() throws CloneNotSupportedException {
+        return new Gate(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) return true;
+        if(obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Gate) obj;
+        return Objects.equals(this.world, that.world) &&
+                this.modifier == that.modifier &&
+                Objects.equals(this.symbol, that.symbol) &&
+                Objects.equals(this.expansion, that.expansion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(world, modifier, symbol, expansion);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("\t\nworld", world)
+                .append("\t\nexpansion", expansion)
+                .append("\t\nmodifier", modifier)
+                .append("\t\nsymbol", symbol)
+                .append("\n")
+                .toString();
+    }
 }
