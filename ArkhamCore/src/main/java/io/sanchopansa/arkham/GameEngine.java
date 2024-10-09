19d681a5a165ee663ba3d25fde93eccdf32bbc1e
SanchoPansa <zelgadis4@gmail.com>
2024-10-09 18:17:44 +0300

    Simple implementation of interfaced Engine

--- ArkhamCore/src/main/java/io/sanchopansa/arkham/GameEngine.java
+++ ArkhamCore/src/main/java/io/sanchopansa/arkham/GameEngine.java
@@ -6,7 +6,6 @@ import io.sanchopansa.arkham.locations.Location;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 
-public class GameEngine {
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -39,27 +38,86 @@ public class GameEngine implements GameEngineInterface {
 
     @Override
     public void launch() {
+        PhaseIterator phaseIterator = new PhaseIterator();
+
+        while(phaseIterator.hasNext() && gameInstance.getDoomLevel() < gameInstance.getAncient().getDoomTrack()) {
+            Phase currentPhase = phaseIterator.next();
+            switch(currentPhase) {
+                case UPKEEP -> upkeep();
+                case MOVEMENT -> movement();
+                case ARKHAM_ENCOUNTER -> encounters();
+                case OTHER_WORLD_ENCOUNTER -> otherWorld();
+                case MYTHOS -> mythos();
+            }
+        }
 
     }
 
     @Override
     public void upkeep() {
+        System.out.println("Upkeep");
+        gameInstance.getPlayers().forEach(Investigator::restoreFocus);
     }
 
     @Override
     public void movement() {
+        System.out.println("Movement");
+
     }
 
     @Override
     public void encounters() {
+        System.out.println("Encounters");
+
     }
 
     @Override
     public void otherWorld() {
+        System.out.println("Other World");
+
     }
 
     @Override
     public void mythos() {
+        this.gameInstance.setDoomLevel(gameInstance.getDoomLevel() + 1);
+        System.out.println("Mythos");
 
+    }
+
+    /**
+     * Итератор обхода фаз.
+     * По умолчанию является бесконечным и требует прерывания снаружи.
+     */
+    private static class PhaseIterator implements Iterator<Phase> {
+        private final List<Phase> phases;
+        private int index = -1;
+
+        public PhaseIterator() {
+            phases = Arrays.stream(Phase.values()).filter(phase -> !phase.equals(Phase.ANY)).toList();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index < Integer.MAX_VALUE;
+        }
+
+        /**
+         * Начать с Мифа, затем идти по предзаданному порядку фаз
+         * Если текущая фаза - Миф, то вернуться к Передышке.
+         * @return Текущая фаза
+         */
+        @Override
+        public Phase next() {
+            Phase returnValue = Phase.MYTHOS;
+            if(index != -1) {
+                returnValue = phases.get(index % phases.size());
+            }
+            index++;
+            return returnValue;
+        }
+
+        public int getIndex() {
+            return index;
+        }
     }
 }
