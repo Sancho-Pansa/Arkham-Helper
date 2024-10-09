fad99b3316d8d16a8da842839f874d0a01940364
SanchoPansa <zelgadis4@gmail.com>
2024-10-09 18:17:01 +0300

    Extracted interface from GameEngine

--- ArkhamCore/src/main/java/io/sanchopansa/arkham/GameEngine.java
+++ ArkhamCore/src/main/java/io/sanchopansa/arkham/GameEngine.java
@@ -1,10 +1,17 @@
 package io.sanchopansa.arkham;
 
+import io.sanchopansa.arkham.common.Phase;
+import io.sanchopansa.arkham.investigators.Investigator;
 import io.sanchopansa.arkham.locations.Location;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 
 public class GameEngine {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class GameEngine implements GameEngineInterface {
     private final Game gameInstance;
     private final GameVault vault;
     private final Graph<Location, DefaultEdge> map;
@@ -15,19 +22,44 @@ public class GameEngine {
         this.map = map;
     }
 
+    @Override
     public Game getGameInstance() {
         return gameInstance;
     }
 
+    @Override
     public GameVault getVault() {
         return vault;
     }
 
+    @Override
     public Graph<Location, DefaultEdge> getMap() {
         return map;
     }
 
+    @Override
     public void launch() {
+
+    }
+
+    @Override
+    public void upkeep() {
+    }
+
+    @Override
+    public void movement() {
+    }
+
+    @Override
+    public void encounters() {
+    }
+
+    @Override
+    public void otherWorld() {
+    }
+
+    @Override
+    public void mythos() {
 
     }
 }
---
+++ ArkhamCore/src/main/java/io/sanchopansa/arkham/GameEngineInterface.java
@@ -0,0 +1,18 @@
+package io.sanchopansa.arkham;
+
+import io.sanchopansa.arkham.locations.Location;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+
+public interface GameEngineInterface {
+    Game getGameInstance();
+    GameVault getVault();
+    Graph<Location, DefaultEdge> getMap();
+    void launch();
+
+    void upkeep();
+    void movement();
+    void encounters();
+    void otherWorld();
+    void mythos();
+}
