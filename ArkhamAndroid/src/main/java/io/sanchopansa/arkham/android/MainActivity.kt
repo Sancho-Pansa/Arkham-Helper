01b0b376c6cc70339414ee1031414b0ada1273c7
SanchoPansa <zelgadis4@gmail.com>
2025-06-26 15:44:48 +0300

    Integrated with Android project in multi-project architecture.

---
+++ ArkhamAndroid/src/main/java/io/sanchopansa/arkham/android/MainActivity.kt
@@ -0,0 +1,52 @@
+package io.sanchopansa.arkham.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.sanchopansa.arkham.Game
+import io.sanchopansa.arkham.android.ui.theme.AndroidTheme
+import io.sanchopansa.arkham.common.Expansion
+import io.sanchopansa.arkham.monsters.DimensionSymbol
+import io.sanchopansa.arkham.monsters.Gate
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val a = Gate(Expansion.VANILLA, "Test", -1, DimensionSymbol.DIAMOND)
+    Text(
+        text = "Hello $a!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidTheme {
+        Greeting("Android")
+    }
+}
